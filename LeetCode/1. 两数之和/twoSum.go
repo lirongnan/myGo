a2fdcd84d4045e954d8211be6502324b5dc62d70
lirongnan <260741104@qq.com>
2021-01-17 22:43:38 +0800

    twoSUm
    Signed-off-by: lirongnan <260741104@qq.com>

---
+++ LeetCode/1. 两数之和/twoSum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums, target = []int{2, 7, 11, 15}, 9
+	var res = twoSum(nums, target)
+	fmt.Print(res)
+}
+
+func twoSum(nums []int, target int) []int {
+	var res = []int{0, 0}
+	for i := 0; i < len(nums)-1; i++ {
+		e := target - nums[i]
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == e {
+				res[0] = i
+				res[1] = j
+				return res
+			}
+		}
+	}
+	return res
+}
